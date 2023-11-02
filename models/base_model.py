3a8080c863a0493d0bd83c195d9613cf1b6e8297
Letho1972 <ericmarescq@gmail.com>
2023-11-02 13:49:54 +0100

    file ok

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,50 @@
 #!/usr/bin/python3
 """
-This is the base model that contains serial/deserial information
+Project AirBnB Clone
 """
 from datetime import datetime
 import uuid
 
 
-class BaseModel():
-    """ Defines all common attributes/methods for other classes """
-    def __init__(self):
-        """ Initializes the instances attributes """
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+class BaseModel:
+    """
+    defines all common attributes
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initializer for BaseModel class
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    date = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """ Prints object in friendly format"""
+        """
+        Returns a string representaton of a BaseModel
+        """
         return "[{}] ({}) {}"\
             .format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """ Updates update_at """
+        """
+        Updates the public instances attribute
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ Generate a new dict with an extra field class"""
+        """
+        returns a dictionary containing all keys/values of __dict__
+        """
         new_dict = self.__dict__.copy()
-        new_dict["class"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
