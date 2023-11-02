c560954c39f2e705e3d538fa7673f1501404cbf7
Letho1972 <ericmarescq@gmail.com>
2023-11-02 13:39:24 +0100

    change return in save func

--- models/base_model.py
+++ models/base_model.py
@@ -7,35 +7,27 @@ import uuid
 
 
 class BaseModel():
-    """
-    Defines all common attributes/methods for other classes
-    """
+    """ Defines all common attributes/methods for other classes """
     def __init__(self):
-        """
-        Initializes the instances attributes
-        """
+        """ Initializes the instances attributes """
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        Prints object in friendly format
-        """
+        """ Prints object in friendly format"""
         return "[{}] ({}) {}"\
-            .format(self.__class__.__name__, self.id, self.__dict__)
+            .format(type(self)__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        Updates update_at
-        """
+        """ Updates update_at """
         self.updated_at = datetime.now()
-        return (self.updated_at)
+        return(self.updated_at)
 
     def to_dict(self):
-        """
-        Generate a new dict with an extra field class
-        """
+        """ Generate a new dict with an extra field class"""
+        new_dict = self.__dict__.copy()
         new_dict["class"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
