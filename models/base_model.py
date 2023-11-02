a2353327654152e0619d1b6c44628c644e7e281a
Letho1972 <ericmarescq@gmail.com>
2023-11-02 12:32:46 +0100

    fix pycodestyle  base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel():
     """ Defines all common attributes/methods for other classes """
     def __init__(self):
         """ Initializes the instances attributes """
-        
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -18,13 +18,12 @@ class BaseModel():
     def save(self):
         """ Updates update_at """
         self.updated_at = datetime.now()
-        return (self.updated_at)      
+        return (self.updated_at)
 
     def __str__(self):
         """ Prints object in friendly format"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
-
 
     def to_dict(self):
         """ Generate a new dict with an extra field class essai """
