dadf8207ccc3927acf09d287f5b2b3185e5e3865
Letho1972 <ericmarescq@gmail.com>
2023-11-03 10:30:31 +0100

    5. Store first object change

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,20 +5,24 @@ import json
 from models.base_model import BaseModel
 import os
 
+
 class FileStorage():
     """class that serializes instances to a JSON file and
     deserializes JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         """returns the dictionary __objects"""
-        return self.__objects
-    
+        return (FileStorage.__objects)
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects
-        
+        class_name = type(obj).__name__
+        my_id = obj.id
+        instance_key = class_name + "." + my_id
+        FileStorage.__objects[instance_key] = obj
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         my_obj_dict = {}
@@ -32,7 +36,7 @@ class FileStorage():
         (only if the JSON file (__file_path) exists"""
         from models.base_model import BaseModel
         my_dict = {
-            "BaseModel": BaseModel,
+            "BaseModel": BaseModel
             }
         if not os.path.isfile(FileStorage.__file_path):
             return
