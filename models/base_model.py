a43242259578153c089a87ec6595e9371653be66
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-05 00:30:53 +0100

    Fix return of def __str__

--- models/base_model.py
+++ models/base_model.py
@@ -32,8 +32,7 @@ class BaseModel:
         """
         Returns a string representaton of a BaseModel
         """
-        return "[{}] ({}) {}"\
-            .format(type(self).__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
     def save(self):
         """
