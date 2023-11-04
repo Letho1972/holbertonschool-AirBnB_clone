ce87842c24a3d505ff40d664244da32f2bfd1566
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-04 22:14:13 +0100

    Unitests for city module

---
+++ tests/test_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""unittest for ity module"""
+
+
+import unittest
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    def test_default_attributes(self):
+        city = City()
+        self.assertEqual(city.state_id, "")
+        self.assertEqual(city.name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
