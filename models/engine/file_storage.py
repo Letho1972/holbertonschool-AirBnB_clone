d9931ecb5abd2bb62448c6bd5115e34337cb29b3
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-05 00:11:05 +0100

    Fix: Uptade if name in my_dict

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,28 +17,45 @@ class FileStorage():
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        my_obj_dict = {}
-        for key in FileStorage.__objects:
-            my_obj_dict[key] = FileStorage.__objects[key].to_dict()
-        with open(FileStorage.__file_path, 'w') as file_path:
-            json.dump(my_obj_dict, file_path)
+        data = {}
+        for key, obj in self.__objects.items():
+            data[key] = obj.to_dict()
+
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(data, file)
 
     def reload(self):
         """deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists"""
-        from models.base_model import BaseModel
+        from models import base_model
+        from models import user
+        from models import place
+        from models import state
+        from models import city
+        from models import amenity
+        from models import review
+
         my_dict = {
-            "BaseModel": BaseModel
-            }
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review
+        }
         if not os.path.isfile(FileStorage.__file_path):
             return
+
         with open(FileStorage.__file_path, "r") as file_path:
             objects = json.load(file_path)
             FileStorage.__objects = {}
             for key in objects:
                 name = key.split(".")[0]
-                FileStorage.__objects[key] = my_dict[name](**objects[key])
+                if name in my_dict:
+                    FileStorage.__objects[key] = my_dict[name](**objects[key])
