a71159a83e7b7d21926cce476dae28cd79afdf96
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-05 15:05:30 +0100

    Import models and add class_dict

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,8 +2,13 @@
 """Module file_storage"""
 
 import json
-import os
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage():
     """class that serializes instances to a JSON file and
@@ -11,6 +16,10 @@ class FileStorage():
 
     __file_path = "file.json"
     __objects = {}
+    class_dict = {"BaseModel": BaseModel, "User": User, "State": State,
+                  "City": City, "Amenity": Amenity, "Place": Place,
+                  "Review": Review,
+                  }
 
     def all(self):
         """returns the dictionary __objects"""
