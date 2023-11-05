0199dbcccaad3d77de874e16cc751f0cda3b2f41
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-05 11:14:13 +0100

    Update def str with .format

--- models/base_model.py
+++ models/base_model.py
@@ -25,14 +25,16 @@ class BaseModel:
                             (value, "%Y-%m-%dT%H:%M:%S.%f"))
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
         """
         Returns a string representaton of a BaseModel
         """
-        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
+        return ("[{}] ({}} {}".
+                format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """
